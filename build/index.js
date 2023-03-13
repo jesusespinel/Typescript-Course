eeadffa09edf6f36c35d91af2f6cd8df0c674d98
Jesus Espinel <you@example.com>
2023-03-13 13:19:51 -0500

    typescript instalation and configuration

---
+++ build/index.js
@@ -0,0 +1,7 @@
+"use strict";
+console.log("Hola Jesús");
+// Este es un comentario en TS.
+/**
+ * Esto es un comentario
+ * Multilinea
+ */ 
---
+++ src/index.ts
@@ -0,0 +1,8 @@
+console.log("Hola Jesús");
+
+// Este es un comentario en TS.
+
+/**
+ * Esto es un comentario
+ * Multilinea
+ */
